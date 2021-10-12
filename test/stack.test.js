147bd1e0a2cac14a86649f359973cfa274a90721
peterchencc <peterchencc@gmail.com>
2021-10-12 18:03:59 +0200

    ✅ Test for the Stack

--- test/stack.test.js
+++ test/stack.test.js
@@ -1,7 +1,45 @@
+class Stack {
+  constructor() {
+    this.top = -1
+    this.items = {}
+  }
+
+  get peek() {
+    return this.items[this.top]
+  }
+
+  push(value) {
+    this.top += 1
+    this.items[this.top] = value
+  }
+
+  pop() {
+    delete this.items[this.top]
+  }
+}
+
 describe('Stack', () => {
+  let stack
 
-  it.todo('is created empty')
-  it.todo('can push to the top')
-  it.todo('can pop off')
+  beforeEach(()=>{
+    stack = new Stack()
+  })
 
+  it('is created empty', () => {
+    expect(stack.top).toBe(-1)
+    expect(stack.items).toEqual({})
+  })
+
+
+  it('can push to the top', () => {
+    stack.push('Apple')
+    expect(stack.top).toBe(0)
+    expect(stack.peek).toBe('Apple')
+  })
+
+  it('can pop off', ()=> {
+    stack.pop()
+    expect(stack.top).toBe(-1)
+    expect(stack.items).toEqual({})
+  })
 })
